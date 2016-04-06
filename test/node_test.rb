ab396e7f38994f57102136b5126ed4e08cdba18f
Kerry <kerry.sheldon@gmail.com>
2016-04-06 08:57:48 -0600

    additional node tests, including integration

--- test/node_test.rb
+++ test/node_test.rb
@@ -1,6 +1,7 @@
 require "minitest/autorun"
 require "minitest/pride"
 require "./lib/node"
+require "./lib/trie"
 
 class NodeTest < MiniTest::Test
   def test_word_false_at_initiation
@@ -8,20 +9,41 @@ class NodeTest < MiniTest::Test
     refute node.word
   end
 
-  def test_word_true_when_set
+  def test_word_true_when_set_directly
     node = Node.new
     node.set_as_word
     assert node.word
   end
 
-  def test_knows_it_has_child
+  def test_word_true_when_set_by_insert
+    trie = Trie.new
+    trie.insert("a")
+    assert trie.root.children["a"].word
+  end
+
+  def test_knows_root_has_child
     node = Node.new
     node.children["a"] = Node.new
     node.children["b"] = Node.new
     assert node.has_child?("a")
     refute node.has_child?("c")
     refute node.children["a"].has_child?("b")
+  end
 
+  def test_knows_non_root_has_child
+    node = Node.new
+    node.children["a"] = Node.new
+    node.children["a"].children["b"] = Node.new
+    assert node.children["a"].has_child?("b")
+    refute node.children["a"].has_child?("c")
+  end
+
+  def test_knows_has_child_when_set_by_insert
+    trie = Trie.new
+    trie.insert("a")
+    trie.insert("ab")
+    assert trie.root.children["a"].has_child?("b")
+    refute trie.root.has_child?("b")
   end
 
   def test_knows_it_has_children
@@ -30,9 +52,12 @@ class NodeTest < MiniTest::Test
     node.children["b"] = Node.new
     assert node.has_children?
     refute node.children["a"].has_children?
-
   end
 
-
+  def test_show_preferences_sorts_by_value_in_array
+    node = Node.new
+    node.preferences = {"apple" => 1, "app" => 8, "all" => 9}
+    assert_equal [9, "all", 8, "app", 1, "apple"], node.show_preferences
+  end
 
 end
